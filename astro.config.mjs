b1aa071cbb05cd09a02b2f2bf0e8bc7255940ef1
kino319 <kino@heise.de>
2025-08-08 12:06:54 +0200

    last changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import preact from '@astrojs/preact';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kinos-astro-tutorial.netlify.app/",
+  integrations: [preact()],
+});
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -7,6 +7,8 @@ image:
     url: 'https://docs.astro.build/assets/rose.webp'
     alt: 'The Astro logo on a dark background with a pink glow.'
 tags: ["astro", "blogging", "learning in public"]
+layout: ../../layouts/MarkdownPostLayout.astro
+
 ---
 # My First Blog Post
 
